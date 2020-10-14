80dd2740b8e78b172aa8a15ad1e32fdd71e8c6a8
GitHub Action <action@github.com>
2020-10-14 01:44:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/googee/laravelgenerator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.googee.laravelgenerator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.googee.laravelgenerator.MyBundle
 
 class MyApplicationService {
 
