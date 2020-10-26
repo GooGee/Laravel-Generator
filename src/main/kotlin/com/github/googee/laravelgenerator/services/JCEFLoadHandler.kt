3ef039ed091fc7caae99555f8d51d556fdf5ead6
GooGee <mou-gj@live.com>
2020-10-26 16:40:48 +0800

    + showError in WindowFactory.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/JCEFLoadHandler.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/JCEFLoadHandler.kt
@@ -1,23 +1,26 @@
 package com.github.googee.laravelgenerator.services
 
-import com.intellij.ui.jcef.JBCefBrowser
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
 import org.cef.handler.CefLoadHandler
 import org.cef.network.CefRequest
 
-class JCEFLoadHandler(val browser: JBCefBrowser, val uri: String, val code: String) : CefLoadHandler {
+class JCEFLoadHandler(val wf: WindowFactory) : CefLoadHandler {
+
     override fun onLoadStart(p0: CefBrowser?, p1: CefFrame?, p2: CefRequest.TransitionType?) {
     }
 
     override fun onLoadingStateChange(p0: CefBrowser?, p1: Boolean, p2: Boolean, p3: Boolean) {
     }
 
-    override fun onLoadError(p0: CefBrowser?, p1: CefFrame?, p2: CefLoadHandler.ErrorCode?, p3: String?, p4: String?) {
+    override fun onLoadError(p0: CefBrowser?, p1: CefFrame?, code: CefLoadHandler.ErrorCode?, text: String?, url: String?) {
+        wf.showError(text ?: "Error")
     }
 
-    override fun onLoadEnd(p0: CefBrowser?, p1: CefFrame?, p2: Int) {
-        browser.cefBrowser.executeJavaScript(code, uri, 0)
-        browser.cefBrowser.executeJavaScript("window.bridge.load()", uri, 0)
+    override fun onLoadEnd(p0: CefBrowser?, p1: CefFrame?, httpStatusCode: Int) {
+        println(httpStatusCode)
+        if (httpStatusCode == 200) {
+            wf.showWeb()
+        }
     }
 }
--- src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
@@ -1,23 +1,53 @@
 package com.github.googee.laravelgenerator.services
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.jcef.JBCefApp
+import com.intellij.ui.jcef.JBCefBrowser
+import com.intellij.ui.layout.panel
+import com.intellij.util.ui.UIUtil
 
 class WindowFactory : ToolWindowFactory {
+    var code: String = ""
+    var browser: JBCefBrowser? = null
+    var window: ToolWindow? = null
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         if (!JBCefApp.isSupported()) {
+            addLabel("JCEF is not supported")
             return
         }
 
         println("create browser")
-        val browser = BrowserFactory.make()
-        val code = QueryManager.register(browser, project)
-        val handler = JCEFLoadHandler(browser, browser.cefBrowser.url, code)
+        window = toolWindow
+        this.browser = BrowserFactory.make()
+        val browser = this.browser!!
+        code = QueryManager.register(browser, project)
+        val handler = JCEFLoadHandler(this)
         browser.jbCefClient.addLoadHandler(handler, browser.cefBrowser)
         toolWindow.component.add(browser.component)
     }
 
+    fun addLabel(text: String) {
+        val panel = panel() {
+            row {
+                label(text, UIUtil.ComponentStyle.LARGE)
+            }
+        }
+        window!!.component.add(panel)
+    }
+
+    fun showError(text: String) {
+        println("showError")
+        addLabel(text)
+    }
+
+    fun showWeb() {
+        println("showWeb")
+        val cefBrowser = browser!!.cefBrowser
+        cefBrowser.executeJavaScript(code, cefBrowser.url, 0)
+        cefBrowser.executeJavaScript("window.bridge.load()", cefBrowser.url, 0)
+    }
 }
