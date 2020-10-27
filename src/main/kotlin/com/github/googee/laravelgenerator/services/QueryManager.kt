35b38581473646e139f3e429749339d6cc9e6e5d
GooGee <mou-gj@live.com>
2020-10-27 11:41:35 +0800

    + getFunction in QueryManager.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/QueryManager.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/QueryManager.kt
@@ -7,48 +7,60 @@ import com.intellij.ui.jcef.JBCefJSQuery
 class QueryManager {
     companion object {
 
+        private fun getFunction(name: String, query: JBCefJSQuery): String {
+            val onSuccessCallback = getSuccessCallback(name)
+            val onFailureCallback = "function(error_code, error_message) {alert(error_message)}"
+            return name + " : function(text) {" + query.inject("text", onSuccessCallback, onFailureCallback) + "}"
+        }
+
+        private fun getSuccessCallback(name: String): String {
+            return "function(response) {window.bridge." + name + "CB(response)}"
+        }
+
         fun register(browser: JBCefBrowser, project: Project): String {
             val fm = FileManager(project)
             val text = load(browser, fm) + "," + save(browser, fm) + "," + make(browser, fm) + "," + readDB(browser, fm)
             return "window.JavaBridge = {$text};"
         }
 
-        fun make(browser: JBCefBrowser, fm: FileManager): String {
+        private fun make(browser: JBCefBrowser, fm: FileManager): String {
             val query = JBCefJSQuery.create(browser)
             query.addHandler { text ->
                 fm.make(text)
                 null
             }
-            return "make : function(text) {" + query.inject("text") + "}"
+            return getFunction("make", query)
         }
 
-        fun readDB(browser: JBCefBrowser, fm: FileManager): String {
+        private fun readDB(browser: JBCefBrowser, fm: FileManager): String {
             val query = JBCefJSQuery.create(browser)
-            query.addHandler { text ->
-                val data = ""
+            query.addHandler { uri ->
+                println(uri)
+                var data = ""
+                Request.get(uri) { sb ->
+                    data = sb.toString()
+                }
                 JBCefJSQuery.Response(data)
             }
-            return "readDB : function() {" + query.inject("") + "}"
+            return getFunction("readDB", query)
         }
 
-        fun load(browser: JBCefBrowser, fm: FileManager): String {
+        private fun load(browser: JBCefBrowser, fm: FileManager): String {
             val query = JBCefJSQuery.create(browser)
             query.addHandler { text ->
                 val data = fm.load()
                 JBCefJSQuery.Response(data)
             }
-            val onSuccessCallback = "function(response) {window.bridge.loadCB(response)}"
-            val onFailureCallback = "function(error_code, error_message) {alert(error_message)}"
-            return "load : function() {" + query.inject("", onSuccessCallback, onFailureCallback) + "}"
+            return getFunction("load", query)
         }
 
-        fun save(browser: JBCefBrowser, fm: FileManager): String {
+        private fun save(browser: JBCefBrowser, fm: FileManager): String {
             val query = JBCefJSQuery.create(browser)
             query.addHandler { text ->
                 fm.save(text)
                 null
             }
-            return "save : function(text) {" + query.inject("text") + "}"
+            return getFunction("save", query)
         }
 
     }
