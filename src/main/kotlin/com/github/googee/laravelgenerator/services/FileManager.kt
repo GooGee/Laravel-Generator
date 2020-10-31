0f57e03211c95396c6d5127b10d062bbc3f00e3f
GooGee <mou-gj@live.com>
2020-10-31 15:43:42 +0800

    -+ FileManager.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/FileManager.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/FileManager.kt
@@ -1,6 +1,6 @@
 package com.github.googee.laravelgenerator.services
 
-import com.github.googee.laravelgenerator.services.json.MakeFile
+import com.github.googee.laravelgenerator.services.json.FileRequest
 import com.google.common.io.CharStreams
 import com.intellij.openapi.project.Project
 import java.io.File
@@ -9,48 +9,60 @@ import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.nio.file.Files
 import java.nio.file.Paths
-import kotlinx.serialization.json.Json
 
 class FileManager(val project: Project) {
     val FileName = "code-generator.json"
+    val ErrorMessage = "Error"
+    val ErrorMessageParse = "Error parsing JSON data"
 
     fun path(file: String): String {
         return project.basePath + File.separator + file
     }
 
-    fun make(text: String) {
-        try {
-            val data = Json.decodeFromString(MakeFile.serializer(), text)
-            val path = Paths.get(path(data.file))
-            Files.createDirectories(path.parent)
-            val writer = PrintWriter(path.toString())
-            writer.println(data.content)
-            writer.close()
-        } catch (error: Exception) {
-            println(error.message)
-        }
-    }
-
-    fun load(): String {
+    fun load(): Response {
         try {
             val stream = FileInputStream(path(FileName))
-            val reader = InputStreamReader(stream)
-            return CharStreams.toString(reader)
+            val reader = InputStreamReader(stream, "UTF-8")
+            return Response.ok("", CharStreams.toString(reader))
         } catch (error: Exception) {
             println(error.message)
+            return Response.error("", "", error.message ?: ErrorMessage)
         }
-
-        return ""
     }
 
-    fun save(text: String) {
-//            println(text)
+    fun read(text: String): Response {
+        var fr: FileRequest? = null
         try {
-            val writer = PrintWriter(path(FileName))
-            writer.println(text)
-            writer.close()
+            fr = FileRequest(text)
+            val stream = FileInputStream(path(fr.file))
+            val reader = InputStreamReader(stream, "UTF-8")
+            return Response.ok(fr.file, CharStreams.toString(reader))
         } catch (error: Exception) {
             println(error.message)
+            if (fr == null) {
+                return Response.error("", "", ErrorMessageParse)
+            }
+            return Response.error(fr.file, "", error.message ?: ErrorMessage)
         }
     }
+
+    fun write(text: String): Response {
+        var fr: FileRequest? = null
+        try {
+            fr = FileRequest(text)
+            val path = Paths.get(path(fr.file))
+            Files.createDirectories(path.parent)
+            val writer = PrintWriter(path.toString())
+            writer.println(fr.data)
+            writer.close()
+            return Response.ok(fr.file, "")
+        } catch (error: Exception) {
+            println(error.message)
+            if (fr == null) {
+                return Response.error("", "", ErrorMessageParse)
+            }
+            return Response.error(fr.file, "", error.message ?: ErrorMessage)
+        }
+    }
+
 }
