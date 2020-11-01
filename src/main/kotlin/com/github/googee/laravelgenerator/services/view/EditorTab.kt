ab9fa77527494b73f1c54a88316ce4206cd8f624
GooGee <mou-gj@live.com>
2020-11-01 22:55:06 +0800

    + EditorTab.kt

---
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/view/EditorTab.kt
@@ -0,0 +1,40 @@
+package com.github.googee.laravelgenerator.services.view
+
+import com.github.googee.laravelgenerator.services.json.JSEditRequest
+import com.intellij.ui.EditorTextField
+import java.awt.BorderLayout
+import javax.swing.JButton
+import javax.swing.JPanel
+
+class EditorTab(data: JSEditRequest, save: (key: String, text: String) -> Unit) : JPanel() {
+
+    init {
+        this.layout = BorderLayout()
+
+        val box = EditorTextField(data.data)
+        box.setOneLineMode(false)
+
+        val saveButton = JButton("Save")
+        saveButton.addActionListener { e ->
+            println("Save")
+            val text = box.text
+            if (text != data.data) {
+                save(data.key, text)
+            }
+        }
+
+        val reset = JButton("Reset")
+        reset.addActionListener { e ->
+            println("Reset")
+            box.text = data.data
+        }
+
+        val panel = JPanel()
+        panel.add(saveButton)
+        panel.add(reset)
+
+        this.add(panel, BorderLayout.PAGE_START)
+        this.add(box, BorderLayout.CENTER)
+    }
+
+}
