da9384347e5bdb980b9e0b7fd5baf9c547fb9c28
GooGee <mou-gj@live.com>
2020-11-01 12:13:07 +0800

    + WebTab in WindowFactory.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
@@ -1,22 +1,20 @@
 package com.github.googee.laravelgenerator.services
 
+import com.github.googee.laravelgenerator.services.view.WebTab
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.jcef.JBCefApp
 
 class WindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val manager = toolWindow.contentManager
+        val em = EditorManager(manager)
         val fm = FileManager(project)
-        val view = View(fm)
-        toolWindow.component.add(view)
-        if (!JBCefApp.isSupported()) {
-            view.showError("Error: JCEF is required")
-            return
-        }
-
-        view.addBrowser()
+        val panel = WebTab(em, fm)
+        val tab = manager.factory.createContent(panel, "Generator", false)
+        tab.isCloseable = false
+        manager.addContent(tab)
     }
 
 }
