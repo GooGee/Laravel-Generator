e3c220fec23adcf6112dc47b0eb3ea9b09646bed
GooGee <mou-gj@live.com>
2020-11-05 16:22:20 +0800

    -+ VirtualFile in EditorFactory.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/view/EditorFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/view/EditorFactory.kt
@@ -2,31 +2,27 @@ package com.github.googee.laravelgenerator.services.view
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.fileTypes.FileTypes
-import com.intellij.openapi.fileTypes.LanguageFileType
-import com.intellij.openapi.fileTypes.StdFileTypes
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFileFactory
+import com.intellij.openapi.vfs.VirtualFileManager
+import java.nio.file.Paths
 
 class EditorFactory {
 
     companion object {
 
-        fun getFileType(type: String): LanguageFileType {
-            if (type == "js") {
-                return StdFileTypes.JS
+        fun make(project: Project, file: String): Editor {
+            val vf = VirtualFileManager.getInstance().refreshAndFindFileByNioPath(Paths.get(file))
+            if (vf == null) {
+                throw Exception("File $file not found!")
             }
-            return FileTypes.PLAIN_TEXT
-        }
 
-        fun make(project: Project, type: String, code: String): Editor {
-            val fileType = getFileType(type)
-            val file = PsiFileFactory.getInstance(project).createFileFromText(fileType.language, code)
-            val document = file.viewProvider.document
+            val document = FileDocumentManager.getInstance().getDocument(vf)
             if (document == null) {
                 throw Exception("Cannot create Editor. document is null!")
             }
-            return EditorFactory.getInstance().createEditor(document, project, fileType, false)
+
+            return EditorFactory.getInstance().createEditor(document, project)
         }
 
     }
