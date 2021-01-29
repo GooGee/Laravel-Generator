1fd08351db64cfef0e4f3100eeabfa5a2adb87ba
GooGee <mou-gj@live.com>
2021-01-29 12:18:48 +0800

    + Constant.kt

---
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/Constant.kt
@@ -0,0 +1,13 @@
+package com.github.googee.laravelgenerator.services
+
+class Constant {
+
+    companion object {
+
+        const val CGFolder = "code-generator"
+
+        const val URI = "https://googee.github.io/Code-Generator"
+
+    }
+
+}
