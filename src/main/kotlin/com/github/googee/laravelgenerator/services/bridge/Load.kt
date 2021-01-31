e7ac8eb9580bf3eeb96db5b2a87e1ca483277a90
GooGee <mou-gj@live.com>
2021-01-31 17:18:23 +0800

    + move in Load.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/bridge/Load.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/bridge/Load.kt
@@ -1,6 +1,7 @@
 package com.github.googee.laravelgenerator.services.bridge
 
 import com.github.googee.laravelgenerator.services.file.FileManager
+import java.nio.file.Files
 
 class Load(val fm: FileManager, val toJS: ToJS) {
 
@@ -12,12 +13,28 @@ class Load(val fm: FileManager, val toJS: ToJS) {
     fun run() {
         val file = fm.getCGFile(FileName)
         try {
+            move(file)
             val text = FileManager.read(file)
             val response = Response.ok(action, key, text)
             toJS.send(response)
         } catch (exception: Exception) {
             val response = Response.error(action, key, "", exception.message)
             toJS.send(response)
+        }
+    }
+
+    /**
+     * move code-generator.json
+     * to code-generator/data.json
+     */
+    private fun move(destination: String) {
+        if (FileManager.isFile(destination)) {
+            return
+        }
+
+        val file = fm.getFullPath("code-generator.json")
+        if (FileManager.isFile(file)) {
+            FileManager.move(file, destination)
         }
     }
 
