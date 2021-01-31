04570c909d47e48c5c932dfe046f33dda538ac96
GooGee <mou-gj@live.com>
2021-01-31 14:11:16 +0800

    -+ MyProjectService.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/MyProjectService.kt
@@ -2,10 +2,32 @@ package com.github.googee.laravelgenerator.services
 
 import com.intellij.openapi.project.Project
 import com.github.googee.laravelgenerator.MyBundle
+import com.github.googee.laravelgenerator.services.bridge.CodeFactory
+import com.github.googee.laravelgenerator.services.bridge.Load
+import com.github.googee.laravelgenerator.services.bridge.ToJS
+import com.github.googee.laravelgenerator.services.bridge.Update
+import com.github.googee.laravelgenerator.services.file.FileManager
+import com.github.googee.laravelgenerator.services.view.BrowserFactory
 
 class MyProjectService(project: Project) {
 
+    val browser = BrowserFactory.make()
+    val toJS = ToJS(browser.cefBrowser)
+    val codeFactory = CodeFactory(browser)
+    val fm = FileManager(project)
+    val load = Load(fm, toJS)
+    val update = Update(fm, toJS)
+
+    companion object {
+
+        private var _instance: MyProjectService? = null
+
+        val instance: MyProjectService
+            get() = _instance as MyProjectService
+    }
+
     init {
         println(MyBundle.message("projectService", project.name))
+        _instance = this
     }
 }
--- src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/WindowFactory.kt
@@ -1,11 +1,5 @@
 package com.github.googee.laravelgenerator.services
 
-import com.github.googee.laravelgenerator.services.bridge.CodeFactory
-import com.github.googee.laravelgenerator.services.bridge.Load
-import com.github.googee.laravelgenerator.services.bridge.ToJS
-import com.github.googee.laravelgenerator.services.bridge.Update
-import com.github.googee.laravelgenerator.services.file.FileManager
-import com.github.googee.laravelgenerator.services.view.BrowserFactory
 import com.github.googee.laravelgenerator.services.view.EditorManager
 import com.github.googee.laravelgenerator.services.view.WebTab
 import com.intellij.openapi.project.Project
@@ -15,15 +9,10 @@ import com.intellij.openapi.wm.ToolWindowFactory
 class WindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val browser = BrowserFactory.make()
-        val toJS = ToJS(browser.cefBrowser)
-        val fm = FileManager(project)
-        val update = Update(fm, toJS)
-        val em = EditorManager(project, toolWindow.contentManager, update)
-        RequestManager.register(em, fm)
-        val codeFactory = CodeFactory(browser)
-        val load = Load(fm, toJS)
-        val panel = WebTab(browser, load, codeFactory)
+        val service = MyProjectService.instance
+        val em = EditorManager(project, toolWindow.contentManager, service.update)
+        RequestManager.register(em, service.fm)
+        val panel = WebTab(service.browser, service.load, service.codeFactory)
         val tab = toolWindow.contentManager.factory.createContent(panel, "Generator", false)
         tab.isCloseable = false
         toolWindow.contentManager.addContent(tab)
