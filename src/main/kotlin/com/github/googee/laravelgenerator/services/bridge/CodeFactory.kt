738dedcc4922d61a024d01279999f50ec9733469
GooGee <mou-gj@live.com>
2021-02-03 10:44:26 +0800

    + handle in CodeFactory.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/bridge/CodeFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/bridge/CodeFactory.kt
@@ -9,6 +9,21 @@ class CodeFactory(val browser: JBCefBrowser) {
 
     private var code = ""
 
+    private fun handle(text: String): Response {
+        var request: Request? = null
+        return try {
+            request = Request.parse(text)
+            println("call Java (${request.action}, ${request.key})")
+            RequestManager.handle(request)
+        } catch (exception: Exception) {
+            if (request != null) {
+                Response.error(request.action, request.key, "", exception.message)
+            } else {
+                Response.error("error", "*", "", exception.message)
+            }
+        }
+    }
+
     private fun makeFunction(query: JBCefJSQuery): String {
         val onSuccessCallback = "function(response) {window.bridge.call(response)}"
         val onFailureCallback = "function(code, message) {window.bridge.error(code, message)}"
@@ -19,10 +34,7 @@ class CodeFactory(val browser: JBCefBrowser) {
     fun make() {
         val query = JBCefJSQuery.create(browser)
         query.addHandler { text ->
-            val request = Request.parse(text)
-            println("call Java (${request.action}, ${request.key})")
-            val json = RequestManager.handle(request).toJSON()
-            JResponse(json)
+            JResponse(handle(text).toJSON())
         }
         val function = makeFunction(query)
         code = "window.JavaBridge = {call : $function};"
