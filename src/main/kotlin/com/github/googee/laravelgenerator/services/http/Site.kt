777cc5ee41c5ec9cb51a4ba7f5240062d886aff9
GooGee <mou-gj@live.com>
2022-02-09 14:30:28 +0800

    + FileManager in Site.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/http/Site.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/http/Site.kt
@@ -1,6 +1,7 @@
 package com.github.googee.laravelgenerator.services.http
 
 import com.github.googee.laravelgenerator.services.Version
+import com.github.googee.laravelgenerator.services.file.FileManager
 
 class Site {
     companion object {
@@ -9,7 +10,14 @@ class Site {
 
         const val URI = "https://googee.github.io/Code-Generator-Page"
 
-        fun getURI(): String {
+        fun getURI(fm: FileManager): String {
+            // build plugin
+            val uriFile = fm.getCGFile(Key)
+            if (FileManager.isFile(uriFile)) {
+                return FileManager.read(uriFile)
+            }
+
+            // runIde
             val uri = System.getenv(Key)
             if (uri.isNullOrEmpty()) {
                 return URI + "/dist" + Version.get()
