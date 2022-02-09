e83061f49d639595ec259061b9f5da7691b4ab7a
GooGee <mou-gj@live.com>
2022-02-09 14:30:14 +0800

    - Site in BrowserFactory.kt

--- src/main/kotlin/com/github/googee/laravelgenerator/services/view/BrowserFactory.kt
+++ src/main/kotlin/com/github/googee/laravelgenerator/services/view/BrowserFactory.kt
@@ -1,12 +1,10 @@
 package com.github.googee.laravelgenerator.services.view
 
-import com.github.googee.laravelgenerator.services.http.Site
 import com.intellij.ui.jcef.JBCefBrowser
 
 class BrowserFactory {
     companion object {
-        fun make(): JBCefBrowser {
-            val uri = Site.getURI() + "?ide=idea"
+        fun make(uri: String): JBCefBrowser {
             println("URI: $uri")
             return JBCefBrowser(uri)
         }
